f63a290bceb7eba465f4a91daddf9f66cff61209
Oliver West <oliversimon.west@gmail.com>
2023-10-17 13:03:13 +0100

    building js class

---
+++ index.js
@@ -0,0 +1,15 @@
+const itemArray = [
+    { name: 'Smarties', code: 'A01', quantity: 10, price: 0.6 },
+    { name: 'Caramac Bar', code: 'A02', quantity: 5, price: 0.6 },
+    { name: 'Dairy Milk', code: 'A03', quantity: 1, price: 0.65 },
+    { name: 'Freddo', code: 'A04', quantity: 1, price: 0.25 },
+    { name: 'Daim', code: 'A05', quantity: 1, price: 0.99 },
+];
+const moneyFloat = 100.0;
+
+class vendingMachine {
+    constructor(items, money) {
+        this.items = items;
+        this.money = money;
+    }
+}
